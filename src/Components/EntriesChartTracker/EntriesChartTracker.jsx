91608c6137c144190c51b31814d55d24af3d3930
Reny Reyes <renyreyes8@hotmail.com>
2022-04-19 17:40:36 -0400

    Moved the legend to the bottom of the EntryChartTracker

--- src/Components/EntriesChartTracker/EntriesChartTracker.jsx
+++ src/Components/EntriesChartTracker/EntriesChartTracker.jsx
@@ -1,22 +1,24 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
 
 const EntriesChartTracker = (props) => {
-
-    const [chartData, setChartData] = useState([])
-useEffect(()=> {
-    let tempChartData = props.parentEntries.map(entry=>{
-        return [entry.date,entry.weight]
-    },[props.parentEntries])
-setChartData(tempChartData)
-})
+  const [chartData, setChartData] = useState([]);
+  useEffect(() => {
+    let tempChartData = props.parentEntries.map(
+      (entry) => {
+        return [entry.date, entry.weight];
+      },
+      [props.parentEntries]
+    );
+    setChartData(tempChartData);
+  });
   return (
     <Chart
       chartType="LineChart"
-      data={[
-        ["Date", "Weight"],...chartData]}
+      data={[["Date", "Weight"], ...chartData]}
       width="100%"
       height="400px"
+      options={{ legend: { position: "bottom" } }}
       legendToggle
     />
   );
